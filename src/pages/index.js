3159282545b3af75295cc3bc967f925df3007d39
Ryan Wiemer <ryan@ryanwiemer.com>
2017-10-16 01:21:42 -0700

    Playing around with the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,55 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Home Page</h1>
+
+  <div className="featured">
+
+    <div className="featured__img">
+      <h1>KNW Photography</h1>
+      <img src="http://knw.io/wp-content/uploads/2017/08/san-ramon-bridges-golf-club-summer-bay-area-wedding-28.jpg" />
+    </div>
+
+    <div className="featured__galleries">
+      <h2>Recent Galleries</h2>
+      <ul>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/08/san-francisco-lifestyle-in-home-newborn-with-dog-7.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/09/fort-funston-foggy-fun-beach-water-engagement-session-35.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/07/key-largo-ocean-reach-indian-fusion-wedding-170.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/07/san-francisco-gloomy-spring-one-year-old-lifestyle-family-session-10.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/08/san-francisco-lifestyle-in-home-newborn-with-dog-7.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/09/fort-funston-foggy-fun-beach-water-engagement-session-35.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/07/key-largo-ocean-reach-indian-fusion-wedding-170.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+        <li>
+          <img src="http://knw.io/wp-content/uploads/2017/07/san-francisco-gloomy-spring-one-year-old-lifestyle-family-session-10.jpg" />
+          <h3>Lorem Ipsum Dolor</h3>
+        </li>
+      </ul>
+    </div>
+
   </div>
+
+</div>
 )
 
 export default IndexPage
