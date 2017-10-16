7e65eea0a13186a7113f23704a4e2805f3d9b105
Ryan Wiemer <ryan@ryanwiemer.com>
2017-10-16 15:36:34 -0700

    Adjustments to the menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,47 +9,38 @@ const IndexPage = () => (
 
     <div className="featured__img">
       <h1>KNW Photography</h1>
-      <img src="http://knw.io/wp-content/uploads/2017/08/san-ramon-bridges-golf-club-summer-bay-area-wedding-28.jpg" />
+      <img src="http://via.placeholder.com/900x600" />
     </div>
 
     <div className="featured__galleries">
       <h2>Recent Galleries</h2>
       <ul>
         <li>
-          <img src="http://knw.io/wp-content/uploads/2017/08/san-francisco-lifestyle-in-home-newborn-with-dog-7.jpg" />
+          <img src="http://via.placeholder.com/900x600" />
           <h3>Lorem Ipsum Dolor</h3>
         </li>
         <li>
-          <img src="http://knw.io/wp-content/uploads/2017/09/fort-funston-foggy-fun-beach-water-engagement-session-35.jpg" />
+          <img src="http://via.placeholder.com/900x600" />
           <h3>Lorem Ipsum Dolor</h3>
         </li>
         <li>
-          <img src="http://knw.io/wp-content/uploads/2017/07/key-largo-ocean-reach-indian-fusion-wedding-170.jpg" />
+          <img src="http://via.placeholder.com/900x600" />
           <h3>Lorem Ipsum Dolor</h3>
         </li>
         <li>
-          <img src="http://knw.io/wp-content/uploads/2017/07/san-francisco-gloomy-spring-one-year-old-lifestyle-family-session-10.jpg" />
+          <img src="http://via.placeholder.com/900x600" />
           <h3>Lorem Ipsum Dolor</h3>
         </li>
         <li>
-          <img src="http://knw.io/wp-content/uploads/2017/08/san-francisco-lifestyle-in-home-newborn-with-dog-7.jpg" />
+          <img src="http://via.placeholder.com/900x600" />
           <h3>Lorem Ipsum Dolor</h3>
-        </li>
-        <li>
-          <img src="http://knw.io/wp-content/uploads/2017/09/fort-funston-foggy-fun-beach-water-engagement-session-35.jpg" />
-          <h3>Lorem Ipsum Dolor</h3>
-        </li>
-        <li>
-          <img src="http://knw.io/wp-content/uploads/2017/07/key-largo-ocean-reach-indian-fusion-wedding-170.jpg" />
-          <h3>Lorem Ipsum Dolor</h3>
-        </li>
-        <li>
-          <img src="http://knw.io/wp-content/uploads/2017/07/san-francisco-gloomy-spring-one-year-old-lifestyle-family-session-10.jpg" />
-          <h3>Lorem Ipsum Dolor</h3>
-        </li>
+        </li>      
       </ul>
     </div>
 
+  </div>
+  <div className="page">
+    <p>Lorem Ipsum dolor</p>
   </div>
 
 </div>
