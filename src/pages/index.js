ad4346a59f61f55e0e225b9e6840c82545e386c5
Ryan Wiemer <ryan@ryanwiemer.com>
2017-10-16 23:39:50 -0700

    More exploration on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,27 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-
-  <div className="featured">
-
-    <div className="featured__img">
+    <div className="intro">
       <h1>KNW Photography</h1>
-      <img src="http://via.placeholder.com/900x600" />
+      <img src="http://knw.io/wp-content/uploads/2017/07/summer-san-francisco-lifestyle-coastal-engagement-session-63.jpg" />
     </div>
 
-    <div className="featured__galleries">
-      <h2>Recent Galleries</h2>
-      <ul>
+    <div className="quote">
+      <blockquote>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </blockquote>
+    </div>
+
+
+    <div className="featured">
+
+      <div className="featured__newest">
+        <h2>Recent Galleries</h2>
+        <img src="http://via.placeholder.com/900x600" />
+        <h3>Lorem Ipsum Dolor</h3>
+      </div>
+
+      <ul className="featured__list">
         <li>
           <img src="http://via.placeholder.com/900x600" />
           <h3>Lorem Ipsum Dolor</h3>
@@ -34,13 +44,19 @@ const IndexPage = () => (
         <li>
           <img src="http://via.placeholder.com/900x600" />
           <h3>Lorem Ipsum Dolor</h3>
-        </li>      
+        </li>
       </ul>
     </div>
 
-  </div>
+    <div className="bio">
+      <div className="bio__inner">
+        <h2>Hi, I'm Kirsten.</h2>
+        <img src="http://via.placeholder.com/900x600" />
+        <Link to="/about">Learn more about me</Link>
+      </div>
+    </div>
+
   <div className="page">
-    <p>Lorem Ipsum dolor</p>
   </div>
 
 </div>
