96c3e65f641cbc91f585f1dc211ec26d7344004e
Ryan Wiemer <ryan@ryanwiemer.com>
2017-10-18 01:22:15 -0700

    More homepage exploration

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,12 @@ const IndexPage = () => (
   <div>
     <div className="intro">
       <h1>KNW Photography</h1>
-      <img src="http://knw.io/wp-content/uploads/2017/07/summer-san-francisco-lifestyle-coastal-engagement-session-63.jpg" />
+      <img src="http://knw.io/wp-content/uploads/2016/03/walking-into-sunset-ocean-beach-sf.jpg" />
     </div>
 
     <div className="quote">
       <blockquote>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Some sort of inspiring snippet of text should go here.
       </blockquote>
     </div>
 
@@ -21,38 +21,37 @@ const IndexPage = () => (
       <div className="featured__newest">
         <h2>Recent Galleries</h2>
         <img src="http://via.placeholder.com/900x600" />
-        <h3>Lorem Ipsum Dolor</h3>
       </div>
 
       <ul className="featured__list">
         <li>
           <img src="http://via.placeholder.com/900x600" />
-          <h3>Lorem Ipsum Dolor</h3>
+          <h3>9.10.2017</h3>
         </li>
         <li>
           <img src="http://via.placeholder.com/900x600" />
-          <h3>Lorem Ipsum Dolor</h3>
+          <h3>9.10.2017</h3>
         </li>
         <li>
           <img src="http://via.placeholder.com/900x600" />
-          <h3>Lorem Ipsum Dolor</h3>
+          <h3>9.10.2017</h3>
         </li>
         <li>
           <img src="http://via.placeholder.com/900x600" />
-          <h3>Lorem Ipsum Dolor</h3>
+          <h3>9.10.2017</h3>
         </li>
         <li>
           <img src="http://via.placeholder.com/900x600" />
-          <h3>Lorem Ipsum Dolor</h3>
+          <h3>9.10.2017</h3>
         </li>
       </ul>
     </div>
 
     <div className="bio">
       <div className="bio__inner">
-        <h2>Hi, I'm Kirsten.</h2>
-        <img src="http://via.placeholder.com/900x600" />
-        <Link to="/about">Learn more about me</Link>
+        <h2>Meet the photographer</h2>
+        <img className="bio__image" src="http://via.placeholder.com/900x600" />
+        <Link to="/about">Learn More About Kirsten &rarr;</Link>
       </div>
     </div>
 
