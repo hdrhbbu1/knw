f09c6b6a39e837c8dfcbfca550c13cdf2953e83a
Ryan Wiemer <ryan@ryanwiemer.com>
2017-10-22 17:51:43 -0700

    Adding next and previous post links

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,35 +1,54 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
+import _ from "lodash"
 
 const PostTemplate = ({data}) => {
-  const post = data.contentfulGallery
+
+  const {
+    title,
+    id,
+    date,
+    slug,
+    description,
+    cover,
+    images
+  } = data.contentfulGallery;
+
+  const postIndex = _.find(
+    data.allContentfulGallery.edges,
+    ({ node: post }) => post.id === id
+  );
+
   return(
     <div className="post">
 
       <div className="post-cover">
-        <Img sizes={post.cover.sizes} alt={post.cover.title} title={post.cover.title} backgroundColor={"#f1f1f1"} />
+        <Img sizes={cover.sizes} alt={cover.title} title={cover.title} backgroundColor={"#f1f1f1"} />
       </div>
 
       <div className="post-info">
         <div className="post-info__left">
           <h2 className="post-info-title">Details</h2>
           <Link to="/galleries"><h3 className="post-category">Wedding</h3></Link>
-          <h3 className="post-title">{post.title}</h3>
-          <h3 className="post-date">{post.date}</h3>
+          <h3 className="post-title">{title}</h3>
+          <h3 className="post-date">{date}</h3>
         </div>
         <div className="post-info__right">
-          <div className="post-description" dangerouslySetInnerHTML={{ __html: post.description.childMarkdownRemark.html }} />
+          <div className="post-description" dangerouslySetInnerHTML={{ __html: description.childMarkdownRemark.html }} />
         </div>
       </div>
 
       <ul className="post-images">
-        {post.images.map((images, index) => (
+        {images.map((images, index) => (
           <li key={index}>
             <Img sizes={images.sizes} alt={images.title} title={images.title} backgroundColor={"#f1f1f1"} />
           </li>
         ))}
       </ul>
+
+      {postIndex.previous && (<Link to={postIndex.previous.slug}>Nex Post</Link>)}
+      {postIndex.next && (<Link to={postIndex.next.slug}>Previous Post</Link>)}
 
     </div>
   )
@@ -58,6 +77,21 @@ export const query = graphql`
         }
       }
     }
+    allContentfulGallery {
+        edges {
+          node {
+            id
+          }
+          previous {
+            slug
+            title
+          }
+          next {
+            slug
+            title
+          }
+        }
+      }
   }
 `
 
