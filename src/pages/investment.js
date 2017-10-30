e86ae026c31032ff7cc79a4c24403e4dfcbee4e1
Ryan Wiemer <ryan@ryanwiemer.com>
2017-10-29 23:42:19 -0700

    More work in progress on the investment page

--- src/pages/investment.js
+++ src/pages/investment.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
 import Helmet from 'react-helmet'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const Investment = ({data}) => {
 
 const tabs = data.allContentfulInvestment.edges;
 
   return(
-    <div className="investment">
+    <div>
 
       <Helmet>
         <title>Investment</title>
@@ -16,19 +17,29 @@ const tabs = data.allContentfulInvestment.edges;
       </Helmet>
 
       <div className="intro--investment">
-        <h2>Investment</h2>
-        <Img sizes={tabs[1].node.cover.sizes} alt={tabs[1].node.cover.title} title={tabs[1].node.cover.title} backgroundColor={"#f1f1f1"} />
+        <div className="intro--investment__image">
+          <Img sizes={tabs[0].node.cover.sizes} alt={tabs[0].node.cover.title} title={tabs[0].node.cover.title} backgroundColor={"#f1f1f1"} />
+        </div>
+        <div className="intro--investment__description">
+          <h2>Investment</h2>
+          <p> Morbi hendrerit vulputate justo, eget blandit libero congue accumsan. Aliquam ac libero gravida, vestibulum diam vitae, pretium felis. Sed scelerisque quam a nunc feugiat tristique. Curabitur vel mi suscipit, sollicitudin lectus quis, molestie purus. Quisque imperdiet lorem est, vel posuere ex tempus vel. Sed eu magna non ex dictum tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+          <p>ullam at sapien in metus sagittis pellentesque. Etiam et tellus consequat, ultrices turpis eu, mattis orci. Aliquam elementum bibendum urna id cursus. Nunc sodales semper augue, vitae mattis elit. Donec suscipit fermentum tristique. Mauris a interdum eros.</p>
+        </div>
       </div>
 
-      <div className="tab">
-        {tabs.map(({ node: tab, index }) => (
-          <div key={tab.id}>
-            <h3>{tab.title}</h3>
-            <blockquote dangerouslySetInnerHTML={{ __html: tab.quote.childMarkdownRemark.html }} />
-          </div>
+      <Tabs className="tabs" selectedTabClassName="tab--selected" selectedTabPanelClassName="tab-panel--selected">
+        <TabList className="tab-list">
+          {tabs.map(({ node: tab, index }) => (
+            <Tab key={tab.id} className="tab">{tab.title}</Tab>
           ))}
-      </div>
+        </TabList>
 
+        {tabs.map(({ node: tab, index }) => (
+          <TabPanel key={tab.id} className="tab-panel">
+            <blockquote dangerouslySetInnerHTML={{ __html: tab.quote.childMarkdownRemark.html }} />
+          </TabPanel>
+        ))}
+      </Tabs>
 
       </div>
   )
@@ -37,7 +48,7 @@ const tabs = data.allContentfulInvestment.edges;
 
 export const query = graphql`
   query InvestmentQuery {
-    allContentfulInvestment {
+    allContentfulInvestment (sort: { fields: [title], order: DESC}) {
       edges {
         node {
           title
