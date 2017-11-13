1fe129208057554cd2d7bb19164c48b95639b9b4
Ryan Wiemer <ryan@ryanwiemer.com>
2017-11-12 23:09:16 -0800

    Changing the default value

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,8 +11,8 @@ class Contact extends React.Component {
     constructor (props) {
       super(props)
       this.state = {
-        guests: 0,
-        budget: 1000,
+        guests: '0',
+        budget: '0',
         reason: '',
         optionalQuestions: false
       };
@@ -72,12 +72,6 @@ class Contact extends React.Component {
               <input type="hidden" name="form-name" value="contact" />
               <input className="form__name" name="name" type="text" placeholder="Full Name" required/>
               <input className="form__email" name="email" type="email" placeholder="Email" required/>
-              <select className="form__reason" name="reason" value={this.state.reason} onChange={this.handleResasonChange} required>
-                <option value="">Reason For Contacting</option>
-                <option value="Book A Lifestyle Package">Book A Lifestyle Package</option>
-                <option value="Book A Wedding Package">Book A Wedding Package</option>
-                <option value="General Inquiry / Question">General Inquiry / Question</option>
-              </select>
               <select className="form__source" name="source" required>
                 <option value="">How Did You Hear About Me?</option>
                 <option value="Referral">Referral</option>
@@ -85,22 +79,24 @@ class Contact extends React.Component {
                 <option value="Google Search">Google Search</option>
                 <option value="Other">Other</option>
               </select>
-
-
-                <div className="form__optional" className={(this.state.optionalQuestions ? "form__optional--visible" : "form__optional")}>
-                  <div className="form__date"><DatePicker name="date" selected={this.state.eventDate} onChange={this.handleDateChange} minDate={moment()} placeholderText="Event Date (Optional)"/></div>
-                  <input className="form__location" name="location" type="text" placeholder="Location / Venue"/>
-                  <div className="form__guests">
-                    <label htmlFor="guests">Number Of Guests?</label> <span>{this.state.guests} Guests</span>
-                    <input name="guests" type="range" value={this.state.guests} onChange={this.handleGuestsChange}  min="0" max="400" step="5" />
-                  </div>
-                  <div className="form__budget">
-                    <label htmlFor="budget">Photography Budget?</label> <span>${this.state.budget}</span>
-                    <input name="budget" type="range" value={this.state.budget} onChange={this.handleBudgetChange}  min="1000" max="10000" step="250" />
-                  </div>
+              <select className="form__reason" name="reason" value={this.state.reason} onChange={this.handleResasonChange} required>
+                <option value="">Reason For Contacting</option>
+                <option value="Book A Lifestyle Package">Book A Lifestyle Package</option>
+                <option value="Book A Wedding Package">Book A Wedding Package</option>
+                <option value="General Inquiry / Question">General Inquiry / Question</option>
+              </select>
+              <div className="form__optional" className={(this.state.optionalQuestions ? "form__optional--visible" : "form__optional")}>
+                <div className="form__date"><DatePicker name="date" selected={this.state.eventDate} onChange={this.handleDateChange} minDate={moment()} placeholderText="Event Date (Optional)"/></div>
+                <input className="form__location" name="location" type="text" placeholder="Location / Venue"/>
+                <div className="form__guests">
+                  <label htmlFor="guests">Number Of Guests?</label> <span>{this.state.guests} Guests</span>
+                  <input name="guests" type="range" value={this.state.guests} onChange={this.handleGuestsChange}  min="0" max="400" step="5" />
                 </div>
-
-
+                <div className="form__budget">
+                  <label htmlFor="budget">Photography Budget?</label> <span>${this.state.budget}</span>
+                  <input name="budget" type="range" value={this.state.budget} onChange={this.handleBudgetChange}  min="1000" max="10000" step="250" />
+                </div>
+              </div>
               <textarea className="form__message" name="message" type="text" placeholder="Message" required></textarea>
               <input className="form__submit" name="submit" type="submit" value="Send" />
               <input type="hidden" name="bot"/>
