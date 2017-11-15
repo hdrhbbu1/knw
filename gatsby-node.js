54dab077b48033ab7735786e6859da5bfc10f049
Ryan Wiemer <ryan@ryanwiemer.com>
2017-11-15 14:52:06 -0800

    testing out redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,27 @@ const { createFilePath } = require('gatsby-source-filesystem')
 const path = require('path')
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
+
+  // Redirects
+  const { createRedirect } = boundActionCreators
+
+  let redirectBatch1 = [
+    { f: `/category/lifestyle`, t: `/lifestyle` },
+    { f: `/category/wedding`, t: '/wedding' },
+    { f: `/pricing`, t: '/investment' },
+    { f: `/pricing/lifestyle`, t: `/investment` },
+    { f: `/pricing/wedding`, t: `/investment` },
+  ]
+
+  for (var { f: f, t: t } of redirectBatch1) {
+    createRedirect({
+      fromPath: f,
+      redirectInBrowser: true,
+      toPath: t,
+    })
+  }
+
+  // Create Post Pages based on post.js template
   const { createPage } = boundActionCreators
   return new Promise((resolve, reject) => {
     graphql(`
