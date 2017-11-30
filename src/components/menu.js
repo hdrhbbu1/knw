37cd88a7101b30996b1461c37690777336347a66
Ryan Wiemer <ryan@ryanwiemer.com>
2017-11-30 01:00:23 -0800

    Testing out new toggle styling

--- src/components/menu.js
+++ src/components/menu.js
@@ -16,8 +16,11 @@ const Menu = () => {
     return (
       <header>
           <button className="toggle" onClick={toggleMenu}>
-            <span></span>
-            <span></span>
+            <span className="toggle__label">Menu</span>
+            <div className="toggle__icon">
+              <span></span>
+              <span></span>
+            </div>
           </button>
           <nav>
             <ul className="site-nav">
