1ea1bb929e8567ad35adeade00fba9f00400ed5e
Ryan Wiemer <ryan@ryanwiemer.com>
2017-12-04 20:32:43 -0800

    Adding a new 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Img from 'gatsby-image'
 import Helmet from 'react-helmet'
 
-const NotFoundPage = () => (
-  <div>
+const NotFoundPage = ({data}) => {
 
-    <Helmet>
-      <title>404</title>
-      <meta name="description" content="" />
-    </Helmet>
+  const page = data.contentfulPage;
 
-    <h2>404. NOT FOUND</h2>
+  return(
+    <div>
 
-  </div>
-)
+      <Helmet>
+        <title>404</title>
+        <meta name="description" content="" />
+      </Helmet>
+
+      <div className="intro--404">
+        <div className="intro--404__box">
+          <h2>Sorry, that page can't be found.<span/> <span/> <span/></h2>
+          <p>Please return <Link to="/">home</Link> or use the menu to navigate to a different page.</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export const query = graphql`
+  query ErrorQuery {
+    contentfulPage(slug: {eq: "contact"}) {
+      title
+      slug
+      id
+      cover {
+        title
+        sizes(maxWidth: 1800) {
+          ...GatsbyContentfulSizes_noBase64
+        }
+      }
+    }
+  }
+`
 
 export default NotFoundPage
